a2e2f0c9341066ee9842f6c740fda5773eea81f4
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-09-24 17:30:54 +0200

    update task 3

--- src/main/kotlin/lms_126/Task3.kt
+++ src/main/kotlin/lms_126/Task3.kt
@@ -1,16 +1,13 @@
 package lms_126
 
 fun task3() {
-
-    TODO("Erase this line and write your solution")
-}
-
-/*
+    /*
 INPUT get heigt
 INPUT get base
 calculet get area
 result area
 
  */
+}
 
 
