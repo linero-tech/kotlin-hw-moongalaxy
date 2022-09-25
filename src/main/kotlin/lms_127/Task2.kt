6d028c517749d2637ee1bdfb3cebbc4edf5ce734
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-09-26 00:10:09 +0200

    task102

--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -2,9 +2,9 @@ package lms_127
 
 fun task2() {
 val name = " Hello Jane"
-    val country = "you are from Sweden"
+    val country = " you are from Sweden"
     val month = " and you were born in June"
-    println("$name, $country$month")
+    println("$name,$country$month")
 
 }
 fun main() {
