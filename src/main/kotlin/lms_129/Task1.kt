19c943835abca2b8c436d8eb63f2769bc67602ef
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-01 20:19:13 +0200

    task 103

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,17 @@
 package lms_129
 
-fun task1(a: Int, b: Int): Int {
+fun task1(a: Int , b: Int): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+
+    if (a < b) {for (number in a..b) {
+        result = number + result
+    }
+    }
+
+    return result
+
+}
+fun main () {
+    println( task1( a = 1, b = 5))
 }
