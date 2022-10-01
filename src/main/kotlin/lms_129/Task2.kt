8044e83af6b4a6008da96d223ab7ac20e66e4470
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-01 20:53:24 +0200

    task103,2

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -2,5 +2,22 @@ package lms_129
 
 fun task2(number: Int): Boolean {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = true
+
+    if (number < 2){
+        result = false
+
+    }else{
+
+        for ( i in 2..number/2){
+            if (number % i == 0){
+                result = false
+                break
+            }
+        }
+    }
+    return result
+}
+fun main () {
+    println(task2( number = 17))
 }
