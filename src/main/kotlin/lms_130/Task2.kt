a39a8871089c5f2e70d0423b647584e0538710ac
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-11 19:51:16 +0200

    Update Task2.kt

--- src/main/kotlin/lms_130/Task2.kt
+++ src/main/kotlin/lms_130/Task2.kt
@@ -1,6 +1,8 @@
 package lms_130
 
 fun task2(items: List<Int>): Int {
+    var result = 4
+
 
     return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
 }
