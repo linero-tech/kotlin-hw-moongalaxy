ce87ff7c9ca7d442a4e124a3ac0d02828cbe87e7
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-15 12:50:35 +0200

    task 9 103

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -5,14 +5,16 @@ fun task9(temperature: String): String {
     val magnitude = temperature.substring(0, temperature.length-1).toDouble()
 
     val isCelsius = temperature.contains("C", ignoreCase = true)
-
     val result = if (isCelsius){
+
         val conversion = ( 9.0 * magnitude / 5.0) + 32.0
         "${conversion.toInt()}f"
+
 
     }else {
         val conversion = (5.0 * (magnitude - 32.0)) / 9.0
         "${conversion.toInt()}c"
+
     }
     return result
 }
