c9574e1af079885fad8c7f48a43adbb662d70ab8
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-16 15:29:09 +0200

    task4104

--- src/main/kotlin/lms_130/Task4.kt
+++ src/main/kotlin/lms_130/Task4.kt
@@ -2,5 +2,21 @@ package lms_130
 
 fun task4(items: List<Int>, factor: Int): List<Int> {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+var result = mutableListOf<Int>()
+
+for (number in items){
+    if (number % factor == 0){
+
+        result.add(number)
+
+    }
 }
+
+return result
+}
+
+fun main (){
+    println(  task4(factor = 2, items = listOf(1, 2, 3, 4, 5, 6)))
+}
+
+
