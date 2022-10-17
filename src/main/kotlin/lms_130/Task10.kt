3ecd749a02ce312262b46cc6addb0504e6f5a171
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-17 08:31:07 +0200

    modul104

--- src/main/kotlin/lms_130/Task10.kt
+++ src/main/kotlin/lms_130/Task10.kt
@@ -1,6 +1,26 @@
 package lms_130
 
 fun task10(items: List<Int>): List<Int> {
+    val result = mutableListOf<Int>()
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var number = 0
+
+    for (i in items){
+        if (i  % 2 == 0){
+            number = i * 2
+            result.add(number)
+
+        }else{ i % 2 == 1
+            number = i * 3
+
+            result.add(number)
+
+        }
+    }
+
+    return result
+
+}
+fun main(){
+    println(task10(items = listOf(1, 2, 3, 4, 5 )))
 }
--- src/main/kotlin/lms_130/Task8.kt
+++ src/main/kotlin/lms_130/Task8.kt
@@ -1,3 +1,20 @@
 package lms_130
 
 fun task8(items: List<Int>): List<Int> {
+
+    var list = items.toSet()
+    var sortedList = list.sorted()
+    val result = sortedList.takeLast(3)
+
+
+
+
+
+
+    return result
+}
+
+fun main(){
+    println(task8(items = listOf(60, 9, 7, 10,20, 40, 120)))
+
+}
