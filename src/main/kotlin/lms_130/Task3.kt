ba2921a7aa3e24cb5c5748e8211b25f4ef8cb0f5
moongalaxy <113828477+moongalaxy@users.noreply.github.com>
2022-10-30 19:36:08 +0100

    104task3

--- src/main/kotlin/lms_130/Task3.kt
+++ src/main/kotlin/lms_130/Task3.kt
@@ -1,21 +1,24 @@
 package lms_130
 
 fun task3(items: List<Int>): List<Int> {
-    val result= listOf<Int>()
 
-    val duplicateitems = mutableListOf<Int>()
-    val uniquelistOfItem = mutableListOf<Int>()
 
-    for (number in items) {
-        if (number in uniquelistOfItem) {
-            duplicateitems.add(number)
-        }else {
-            uniquelistOfItem
+    var result = mutableSetOf<Int>()
+    val uniqueListOf = mutableListOf<Int>()
+    for (number in items){
+        if (number in uniqueListOf){
+            result.add(number)
+        }else{
+            uniqueListOf.add(number)
         }
+
     }
 
-    return result
+
+    return result.toList()
 }
-fun main (){
-    println(task3(items = listOf(1, 1, 1, 2, 2, 3)) )
+
+fun main () {
+    println( task3(items = listOf(1, 1, 1, 2, 2, 3 )))
 }
+
